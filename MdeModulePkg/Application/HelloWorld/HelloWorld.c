b637b1058e8f7c8d24d3899d030a8a9a7723649f
Nick Wang <nick.wang@insyde.com>
2025-04-15 18:18:24 +0800

    MdeModulePkg : Peek keyboard queue buffer
    Add test code in HelloWorld UEFI application to peek the last user input
    keystroke data from keyboard queue buffer via the
    EFI_SIMPLE_TEXT_INPUT_PROTOCOL pointer.
    
    Signed-off-by: Nick Wang <nick.wang@insyde.com>

--- MdeModulePkg/Application/HelloWorld/HelloWorld.c
+++ MdeModulePkg/Application/HelloWorld/HelloWorld.c
@@ -11,6 +11,12 @@
 #include <Library/PcdLib.h>
 #include <Library/UefiLib.h>
 #include <Library/UefiApplicationEntryPoint.h>
+#include <Library/UefiBootServicesTableLib.h>
+#include <Library/DebugLib.h>
+#include <Protocol/DevicePath.h>
+#include <Protocol/SimpleTextIn.h>
+#include <Protocol/SimpleTextInEx.h>
+#include <Protocol/UsbIo.h>
 
 //
 // String token ID of help message text.
@@ -21,6 +27,90 @@
 // Shell.
 //
 GLOBAL_REMOVE_IF_UNREFERENCED EFI_STRING_ID  mStringHelpTokenId = STRING_TOKEN (STR_HELLO_WORLD_HELP_INFORMATION);
+
+//
+// Copy the USB_KB_DEV definition from EfiKey.h
+//
+#define MAX_KEY_ALLOWED                 64
+
+typedef struct {
+  VOID          *Buffer[MAX_KEY_ALLOWED + 1];
+  UINTN         Head;
+  UINTN         Tail;
+  UINTN         ItemSize;
+} USB_SIMPLE_QUEUE;
+
+typedef struct {
+  UINTN                                 Signature;
+  EFI_HANDLE                            ControllerHandle;
+  EFI_DEVICE_PATH_PROTOCOL              *DevicePath;
+  EFI_SIMPLE_TEXT_INPUT_PROTOCOL        SimpleInput;
+  EFI_USB_IO_PROTOCOL                   *UsbIo;
+  EFI_USB_INTERFACE_DESCRIPTOR          InterfaceDescriptor;
+  EFI_USB_ENDPOINT_DESCRIPTOR           IntEndpointDescriptor;
+  USB_SIMPLE_QUEUE                      UsbKeyQueue;
+  USB_SIMPLE_QUEUE                      EfiKeyQueue;
+} USB_KB_DEV;
+
+#define USB_KB_DEV_SIGNATURE            SIGNATURE_32 ('u', 'k', 'b', 'd')
+#define USB_KB_DEV_FROM_THIS(a)         CR(a, USB_KB_DEV, SimpleInput, USB_KB_DEV_SIGNATURE)
+
+/**
+  Try to peek at the last user input character from the USB keyboard via the EFI_SIMPLE_TEXT_INPUT_PROTOCOL pointer.
+**/
+VOID
+PeekLastCharFromUsbKbDev (
+  VOID
+  )
+{
+  EFI_STATUS                            Status;
+  UINTN                                 Index;
+  UINTN                                 HandleCount;
+  EFI_HANDLE                            *HandleBuffer;
+  EFI_USB_IO_PROTOCOL                   *UsbIo;
+  EFI_SIMPLE_TEXT_INPUT_PROTOCOL        *SimpleInput;
+  USB_KB_DEV                            *UsbKbDev;
+  EFI_KEY_DATA                          KeyData;
+
+  HandleCount   = 0;
+  HandleBuffer  = NULL;
+  Status = gBS->LocateHandleBuffer (
+                  ByProtocol,
+                  &gEfiSimpleTextInProtocolGuid,
+                  NULL,
+                  &HandleCount,
+                  &HandleBuffer
+                  );
+  if (EFI_ERROR (Status)) {
+    return;
+  }
+
+  for (Index = 0; Index < HandleCount; Index++) {
+    Status = gBS->HandleProtocol (HandleBuffer[Index], &gEfiUsbIoProtocolGuid, (VOID **) &UsbIo);
+    if (EFI_ERROR (Status)) {
+      continue;
+    }
+
+    Status = gBS->HandleProtocol (HandleBuffer[Index], &gEfiSimpleTextInProtocolGuid, (VOID **) &SimpleInput);
+    if (EFI_ERROR (Status)) {
+      continue;
+    }
+
+    UsbKbDev = USB_KB_DEV_FROM_THIS(SimpleInput);
+    break;
+  }
+  gBS->FreePool (HandleBuffer);
+  if (UsbKbDev == NULL) {
+    return;
+  }
+
+  if (UsbKbDev->EfiKeyQueue.Head == 0) {
+    CopyMem (&KeyData, UsbKbDev->EfiKeyQueue.Buffer[MAX_KEY_ALLOWED], sizeof(KeyData));
+  } else {
+    CopyMem (&KeyData, UsbKbDev->EfiKeyQueue.Buffer[UsbKbDev->EfiKeyQueue.Head - 1], sizeof(KeyData));
+  }
+  Print (L"USB KB Last Char=%c\n", KeyData.Key.UnicodeChar);
+}
 
 /**
   The user Entry Point for Application. The user code starts with this function
@@ -55,6 +145,8 @@ UefiMain (
       Print ((CHAR16 *)PcdGetPtr (PcdHelloWorldPrintString));
     }
   }
+
+  PeekLastCharFromUsbKbDev ();
 
   return EFI_SUCCESS;
 }
